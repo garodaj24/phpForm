ea5f74be18fe54522287fc4e9c45e4b2e2c83da4
garodaj24 <garod700@gmail.com>
2020-11-03 17:00:55 +0400

    change comment data type

--- forms_users.sql
+++ forms_users.sql
@@ -26,9 +26,9 @@ CREATE TABLE `users` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `fullname` varchar(45) NOT NULL,
   `email` varchar(45) NOT NULL,
-  `comment` varchar(45) DEFAULT NULL,
+  `comment` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,6 +37,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Garod Ajemian','garod700@gmail.com','hello there my name is Garod!'),(2,'Garod','garod@test.com','hello world! @@@');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -49,4 +50,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-11-03 16:10:27
+-- Dump completed on 2020-11-03 16:59:18
